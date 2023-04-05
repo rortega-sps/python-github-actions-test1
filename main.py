e04d0c5abcea4bc1688c1bdf0fb4fe3b944295f1
JosueCruzBarragan <jcruz@spsolutions.com.mx>
2023-04-05 12:12:30 -0600

    Token add
    Se añadió la variable de entorno

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+
 def main():
     print('Hellow from GitHub Actions!')
+    token = os.environ.get("AWS_TOKEN")
+    if not token:
+        raise RuntimeError("AWS_TOKEN env var is not set!")
+    print("All good! we found our env var!")
 
 
 if __name__ == '__main__':
